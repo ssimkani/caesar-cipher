ac11f2b552a4f0ad82a64bc4304f18285805f138
Seeba184 <seenasimkani@gmail.com>
2024-04-24 10:57:29 -0400

    feat: add map enumerable to assign new string values

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,14 @@
 def caesar_cipher(string, key)
   alphabet = [*"a".."z", *"a".."z"]
   string_arr = string.split("")
+
+  string_arr.map! do |char|
+    if !alphabet.include?(char.downcase)
+      char
+    elsif char == char.upcase
+      alphabet[(alphabet.index(char.downcase) + key) % 26].upcase
+    else
+      alphabet[(alphabet.index(char.downcase) + key) % 26]
+    end
+  end
 end
