d62303bce9e82fe8a07107c4ccdae03dd68a2612
Seeba184 <seenasimkani@gmail.com>
2024-04-24 11:20:04 -0400

    feat: add comments and docstring

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,16 @@
 def caesar_cipher(string, key)
+  # Encrypts the given string using the Caesar Cipher
+  #
+  # @param string [String] The string to encrypt
+  # @param key [Integer] The amount to shift each letter by
+  # @return [String] The encrypted string
+
   alphabet = [*"a".."z"]
+
+  # turning string into array
   string_arr = string.split("")
 
+  # map method for encryption
   string_arr.map! do |char|
     if !alphabet.include?(char.downcase)
       char
@@ -11,7 +20,7 @@ def caesar_cipher(string, key)
       alphabet[(alphabet.index(char.downcase) + key) % 26]
     end
   end
+
+  #return encrypted string
   string_arr.join
 end
-
-p caesar_cipher("What a string!", -13)
