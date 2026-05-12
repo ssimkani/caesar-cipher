31e1546a8ec73ed077c8895267b7e51bc30d100e
ssimkani <seenasimkani@gmail.com>
2026-05-11 22:26:32 -0400

    Add class to caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-def caesar_cipher(string, key)
-  alphabet = [*"a".."z"]
+class CaesarCipher
+  def encrpyt(string, key)
+    alphabet = [*"a".."z"]
 
-  # turning string into array
-  string_arr = string.split("")
+    # turning string into array
+    string_arr = string.split("")
 
-  # map method for encryption
-  string_arr.map! do |char|
-    if !alphabet.include?(char.downcase)
-      char
-    elsif char == char.upcase
-      alphabet[(alphabet.index(char.downcase) + key) % 26].upcase
-    else
-      alphabet[(alphabet.index(char.downcase) + key) % 26]
+    # map method for encryption
+    string_arr.map! do |char|
+      if !alphabet.include?(char.downcase)
+        char
+      elsif char == char.upcase
+        alphabet[(alphabet.index(char.downcase) + key) % 26].upcase
+      else
+        alphabet[(alphabet.index(char.downcase) + key) % 26]
+      end
     end
-  end
 
-  # return encrypted string
-  string_arr.join
+    # return encrypted string
+    string_arr.join
+  end
 end
